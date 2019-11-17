2d863a20786915304b201170090168b5d2cd9cb5
Jeffrey Jordan <jefljord@ut.utm.edu>
2019-11-17 12:40:10 -0600

    Add project files.

---
+++ SBI Mgt System/Room1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SBI_Mgt_System
+{
+    public partial class Room1 : Form
+    {
+        public Room1()
+        {
+            InitializeComponent();
+        }
+    }
+}
